512e831ff5bc31aa9ad7a907c590a9dcefdc8386
SteveJeong <jst0930@gmail.com>
2022-07-01 16:49:20 +0300

    Python 입력 Frontend

--- src/App.svelte
+++ src/App.svelte
@@ -1,65 +1,55 @@
 <script>
-  import logo from './assets/svelte.png'
-  import Counter from './lib/Counter.svelte'
+  let inputVar1=''
+  let inputVar2=''
+  let inputVar3=''
+
+  $: output = inputVar1 + inputVar2 + inputVar3;
+
+  const handleSubmit = () => {
+    window.open(output, '_blank').focus();
+  }
 </script>
 
 <main>
-  <img src={logo} alt="Svelte Logo" />
-  <h1>Hello world!</h1>
-
-  <Counter />
-
-  <p>
-    Visit <a href="https://svelte.dev">svelte.dev</a> to learn how to build Svelte
-    apps.
-  </p>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme">SvelteKit</a> for
-    the officially supported framework, also powered by Vite!
-  </p>
+  <h1>Python 입력 Frontend</h1>
+  <div class="inputFrame">
+    <h2>입력</h2>
+    <div>
+      <label for="inputVar1">입력변수 1</label>
+      <input 
+        name="inputVar1" 
+        type="text" 
+        placeholder="type here ..." 
+        bind:value={inputVar1}
+      />
+    </div>
+    <div>
+      <label for="inputVar2">입력변수 2</label>
+      <input 
+        name="inputVar2" 
+        type="text" 
+        placeholder="type here ..." 
+        bind:value={inputVar2}
+      />
+    </div>
+    <div>
+      <label for="inputVar3">입력변수 3</label>
+      <input 
+        name="inputVar3" 
+        type="text" 
+        placeholder="type here ..." 
+        bind:value={inputVar3}
+      />
+    </div>
+  </div>
+  <div class="outputFrame">
+    <h2>출력</h2>
+    <p>{output}</p>
+    <div>
+      <button on:click={handleSubmit}>제출</button>
+    </div>
+  </div>
 </main>
 
 <style>
-  :root {
-    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
-      Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-  }
-
-  main {
-    text-align: center;
-    padding: 1em;
-    margin: 0 auto;
-  }
-
-  img {
-    height: 16rem;
-    width: 16rem;
-  }
-
-  h1 {
-    color: #ff3e00;
-    text-transform: uppercase;
-    font-size: 4rem;
-    font-weight: 100;
-    line-height: 1.1;
-    margin: 2rem auto;
-    max-width: 14rem;
-  }
-
-  p {
-    max-width: 14rem;
-    margin: 1rem auto;
-    line-height: 1.35;
-  }
-
-  @media (min-width: 480px) {
-    h1 {
-      max-width: none;
-    }
-
-    p {
-      max-width: none;
-    }
-  }
 </style>
